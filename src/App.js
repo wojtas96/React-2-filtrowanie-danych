e248cbdb23d976733665ac3c9f8277f429952dfe
Klaudia <koliber96k@gmail.com>
2024-07-04 18:31:10 +0200

    React-2-filtrowanie-danych

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import './UsersList.css';
+import UserList from './UsersList.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>
+          FILTROWANIE DANYCH W REACT JAVASCRIPT
+        </h2>
       </header>
+      <UserList />
     </div>
   );
-}
+};
 
 export default App;
